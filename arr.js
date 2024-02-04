aed018d5d326fa64b19ab016fe1a960c1482f4bc
collins rop <145935789+collinsrop@users.noreply.github.com>
2024-02-04 21:59:17 +0300

    Add files via upload

---
+++ arr.js
@@ -0,0 +1,8 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", ()=>{
+const theArray= [2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024];
+document.querySelector("button").addEventListener("click", ()=>{
+	let sumation= theArray.reduce((current, total)=>total-current,0);
+	console.log(sumation);
+});
+});
